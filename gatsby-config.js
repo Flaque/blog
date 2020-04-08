afc2fac95d89a5ca566294ad90417b39d62fa1e5
Flaque <evanjamesconrad@gmail.com>
2020-04-08 13:43:27 -0700

    Add blog stuffg

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+
+module.exports = {
+  siteMetadata: {
+    title:
+      "Evan Conrad this is the website of Evan Conrad a person named Evan Conrad who is currently the founder of Room Service",
+    description:
+      "This is the website of Evan Conrad the founder of Room Service and creator of dumb projects",
+    author: "@evanjconrad",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: { default: path.resolve("./src/components/layout.js") },
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "gatsby-default-mdx-basic",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/derp.png", // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+};
